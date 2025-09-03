f867971c400e96285547fc7193542eba14ee6c7a
AstraSpec <rizvan.faisal@gmail.com>
2025-09-03 17:18:27 +0100

    Splits fetch functions into separate files

--- R/get_csv_data.R
+++ R/get_csv_data.R
@@ -1,18 +1,3 @@
-get_csv_data <- function(query = "debt") {
-  url <- paste0("https://data.gov.uk/api/3/action/package_search?q=", query)
-  res <- httr::GET(url)
-  httr::stop_for_status(res)
-  
-  content <- httr::content(res, as = "parsed", type = "application/json")
-  datasets <- content$result$results
-  
-  tibble::tibble(
-    title = purrr::map_chr(datasets, "title", .default = NA_character_),
-    name  = purrr::map_chr(datasets, "name", .default = NA_character_),
-    url   = purrr::map_chr(datasets, "url", .default = NA_character_)
-  )
-}
-
 get_csv_data <- function(data) {
   # ensure no NA urls
   valid <- dplyr::filter(data, !is.na(name))
@@ -34,7 +19,3 @@ get_csv_data <- function(data) {
       dplyr::filter(!is.na(csv_url), toupper(format) == "CSV")
   })
 }
-
-a <- get_package()
-b <- get_csv_data(a)
-print(b)
