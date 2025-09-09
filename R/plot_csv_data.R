da412c8be6d2343ddb1b762a534ff2feaf9090a0
AstraSpec <rizvan.faisal@gmail.com>
2025-09-09 15:39:49 +0100

    Switched plotting to bar chart

--- R/plot_csv_data.R
+++ R/plot_csv_data.R
@@ -3,8 +3,7 @@ plot_csv_data <- function(df, x_col, y_col, title = "Data Plot") {
   stopifnot(x_col %in% names(df), y_col %in% names(df))
   
   ggplot2::ggplot(df, ggplot2::aes(x = .data[[x_col]], y = .data[[y_col]])) +
-    ggplot2::geom_line(color = "1380A1", linewidth = 1) +
-    ggplot2::geom_point(color = "333333", size = 2) +
+    ggplot2::geom_col(fill = "#1380A1", color = "#333333", width = 0.7) +
     ggplot2::labs(
       x = x_col,
       y = y_col,
