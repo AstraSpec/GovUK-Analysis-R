ab9de98c48644f3fd5776495bea8c7aa71092e58
AstraSpec <rizvan.faisal@gmail.com>
2025-09-13 14:32:11 +0100

    Adds documentation arguments

--- R/get_package.R
+++ R/get_package.R
@@ -12,11 +12,3 @@ get_package <- function(query = "debt") {
     url   = purrr::map_chr(datasets, "url", .default = NA_character_)
   )
 }
-
-#packages <- get_package("debt")
-#csvs <- get_csv_data(packages)
-#csv <- csvs$csv_url[1]
-#data <- load_csv_data(csv)
-#clean_data <- clean_csv_data(data)
-#p <- plot_csv_data(clean_data, "InvoiceMonth", "MeatIndustry", "Data set")
-#print(p)
